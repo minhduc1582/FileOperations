e4ee5b9b5d1331bc1e1b1d1b3fcb67ec6cf56fbf
Marik.Nguyen <marik.nguyen@avepoint.com>
2023-08-14 15:47:55 +0700

    turn in

---
+++ Marik_File/Marik_File/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marik_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = @"../../input.txt";
+            string outputFile = @"../../output.txt";
+
+            try
+            {
+                string content = File.ReadAllText(inputFile);
+                string capitalizedContent = CapitalizeWords(content);
+
+                File.WriteAllText(outputFile, capitalizedContent);
+
+                Console.WriteLine("File successfully processed!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while processing the file:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        static string CapitalizeWords<T>(T input)
+        {
+            string content = input.ToString();
+
+            return content.ToUpper();
+        }
+    }
+}
